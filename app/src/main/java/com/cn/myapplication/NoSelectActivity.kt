4a03dfe3667be6f3845d3e2e83b0c206f8f2d3e3
zhaojiaxing <zhaojiaxing_name@163.com>
2019-07-25 08:56:32 +0800

    kotlin协程

--- app/src/main/java/com/cn/myapplication/NoSelectActivity.kt
+++ app/src/main/java/com/cn/myapplication/NoSelectActivity.kt
@@ -2,6 +2,10 @@ package com.cn.myapplication
 
 import android.os.Bundle
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 
 /**
  * @author: zhaojiaxing@gmail.com
@@ -16,9 +20,24 @@ class NoSelectActivity : RxAppCompatActivity() {
         println(selectType(0))
         selectType(1)
         selectType(2)
+        loadData()
     }
 
     private fun selectType(type: Int): String {
-        return "(●￣(ｴ)￣●)"
+        return "(●￣(ｴ)￣●)" + Thread.currentThread().id
+    }
+
+    private fun loadData() = launch(UI) {
+        println("执行异步任务之前：Thread_id = " + Thread.currentThread().id)
+        val result1 = async(CommonPool) {
+            println("执行异步任务1：Thread_id = " + Thread.currentThread().id)
+            return@async "我是任务1"
+        }.await()
+        val result2 = async(CommonPool) {
+            println("执行异步任务2：Thread_id = " + Thread.currentThread().id)
+            return@async "我是任务2"
+        }.await()
+        val result = "$result1 $result2" // ui thread
+        println("执行异步任务之后：result = $result")
     }
 }
